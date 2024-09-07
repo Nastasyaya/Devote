85e434aa9c4f950b1f14b9a5d1ab0fb703038492
Anastasiia <nastyak25555@gmail.com>
2024-09-07 10:37:16 +0200

    Import widget API

---
+++ DevoteWidget/DevoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DevoteWidgetBundle.swift
+//  DevoteWidget
+//
+//  Created by Анастасия Кутняхова on 07.09.2024.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DevoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DevoteWidget()
+    }
+}
